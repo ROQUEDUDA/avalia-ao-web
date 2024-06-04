2088a4693e968c945ed261729d663fe057a6b18c
MARIA EDUARDA ROQUE <00001107786277sp@aluno.educacao.sp.gov.br>
2024-06-04 10:16:50 -0300

    Atualizando o clone com o original alterado

--- script.js
+++ script.js
@@ -4,41 +4,75 @@ function dividirNumeros(){
     var resultadoDividir = numero1 / numero2
     var teste = document.getElementById("resultadoDividir")
     teste.innerHTML=resultadoDividir
-    if ((numero2==0)){
-        alert("Seu numero não pode ser 0, digite outro numero")}
-}
-
-function mediaNotas(){
-    var numero03 = document.getElementById("num03").value;
-    var numero03Alterado=parseInt(numero03);
-    var numero04 = document.getElementById("num04").value;
-    var numero04Alterado=parseInt(numero04);
-    var numero05 = document.getElementById("num05").value;
-    var numero05Alterado=parseInt(numero05);
-    var numero06 = document.getElementById("num06").value;
-    var numero06Alterado=parseInt(numero06);
-    var resultado = numero03Alterado+numero04Alterado+numero05Alterado+numero06Alterado
-    var media = resultado/4
-    if ((media>5))
-    alert("Aluno aprovado, sua média é de"+media);
-    else ((media<5))
-    alert("Aluno retido, sua média é de"+media);
-}
-
-function somarNumeros(){
-    var numero7 = parseInt(document.getElementById("num7").value);
-    var numero8 = parseInt(document.getElementById("num8").value);
-    var numero9 = parseInt(document.getElementById("num9").value);
-    var resultado = numero7 + numero8
-    if ((numero9<resultado))
-        alert(resultado+" é maior que "+numero9);
-    else ((numero9>resultado))
-        alert (resultado=" é menor que "+numero9);
-}
-
-function retangulo(){
-    var numero10 = parseInt(document.getElementById("num10").value);
-    var numero11 = parseInt(document.getElementById("num11").value);
-    var resultado = numero10 * numero11
-    var teste = document.getElementById("resultadoretangulo");
-}
+    if((numero2==0)){
+        alert("seu numero n pode ser 0, digite outro")
+    }
+  }
+  
+  function notaNumeros(){
+    var nota1 = document.getElementById("nota1").value
+    var nota1Alterado = parseInt(nota1)
+    var nota2 = document.getElementById("nota2").value
+    var nota2Alterado = parseInt(nota2)
+    var nota3 = document.getElementById("nota3").value
+    var nota3Alterado = parseInt(nota3)
+    var nota4 = document.getElementById("nota4").value
+    var nota4Alterado = parseInt(nota4)
+  
+    var resultado = (nota1Alterado+nota2Alterado+nota3Alterado+nota4Alterado)/4
+  
+    if(resultado>5){
+        alert("o aluno foi aprovado com: "+resultado)
+    }
+  
+    else{
+        alert("O aluno foi reprovado com: "+resultado)
+    }
+  
+  }
+  
+  function somarNumeros(){
+    var numero1 = document.getElementById("num10").value
+    var numero1Alterado = parseInt(numero1)
+    var numero2 = document.getElementById("num11").value
+    var numero2Alterado = parseInt(numero2)
+    var numero3 = document.getElementById("num12").value
+    var numero3Alterado = parseInt(numero3)
+    var resultado = numero1Alterado+numero2Alterado
+    if(numero3Alterado<resultado){
+        alert(resultado+ " é maior que " +numero3Alterado)
+    }
+    else{
+        alert(resultado+ " é menor que " +numero3Alterado)
+    }
+  
+  }
+  
+  function retangulo(){
+    
+    var numero13 = parseInt(document.getElementById("num13").value);
+    var numero14 = parseInt(document.getElementById("num14").value);
+    var resultado = numero13 * numero14
+    var teste = document.getElementById("resultadoretangulo")
+    teste.innerHTML=resultado
+  }
+  
+  function reajuste(){
+    var numero15 = parseInt(document.getElementById("num15").value);
+    var numero16 = parseInt(document.getElementById("porc").value);
+    var resultado = numero15*(numero16/100)+numero15
+    var teste = document.getElementById("salarioAtual")
+    teste.innerHTML=resultado
+    
+  }
+  
+  function pinto(){
+    var numero17 = parseInt(document.getElementById("num17").value);
+    if(numero17<10){
+        alert("o numero digitado é menor que 10")
+    }
+    else{
+        alert("o numero digitado é maior que 10")
+    }
+    
+  }
